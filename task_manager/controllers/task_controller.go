ba379a92ac8df6788abc29d4d1c25fb6a594fe95
unknown <emelkamu9@gmail.com>
2024-07-31 17:57:14 +0300

    Added Comments

--- task_manager/controllers/task_controller.go
+++ task_manager/controllers/task_controller.go
@@ -8,14 +8,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// TaskController is a struct that holds a pointer to the TaskService.
 type TaskController struct {
 	service *data.TaskService
 }
 
+// NewTaskController creates a new instance of TaskController.
 func NewTaskController(service *data.TaskService) *TaskController {
 	return &TaskController{service}
 }
 
+// GetTasks is a handler function that returns all tasks.
 func (tc *TaskController) GetTasks(ctx *gin.Context) {
 	tasks := tc.service.GetTasks()
 
@@ -25,32 +28,40 @@ func (tc *TaskController) GetTasks(ctx *gin.Context) {
 	})
 }
 
+// GetTaskByID is a handler function that returns a task by ID.
 func (tc *TaskController) GetTaskByID(ctx *gin.Context) {
 	taskID := ctx.Param("id")
 	task := tc.service.GetTaskByID(taskID)
 
+	// If the task is not found, return a 404 response.
 	if task == nil {
 		ctx.JSON(404, gin.H{"error": "Task not found"})
 		return
 	}
 
+	// Otherwise, return a 200 response with the task.
 	ctx.JSON(200, task)
 }
 
 func (tc *TaskController) CreateTask(ctx *gin.Context) {
 	newTask := models.Task{}
 
+	// Bind the request body to the newTask struct.
 	err := ctx.BindJSON(&newTask)
+
+	// If there is an error binding the request body, return a 400 response.
 	if err != nil {
 		ctx.JSON(400, gin.H{"error": "Invalid request!"})
 		return
 	}
 
+	// If the ID field is present in the request body, return a 400 response.
 	if newTask.ID != "" {
 		ctx.JSON(400, gin.H{"error": "id field is not allowed"})
 		return
 	}
 
+	// If any of the required fields are missing, return a 400 response.
 	if newTask.Title == "" {
 		ctx.JSON(400, gin.H{"error": "title field is required"})
 		return
@@ -61,25 +72,34 @@ func (tc *TaskController) CreateTask(ctx *gin.Context) {
 		return
 	}
 
+	// If the status field is missing, set it to "Pending" by default.
 	if newTask.Status == "" {
 		newTask.Status = "Pending"
 	}
 
+	// Generate a new ID for the task.
 	newTask.ID = tc.service.GenerateID()
 
+	// Create the task using the TaskService.
 	tc.service.CreateTask(&newTask)
+
+	// Return a 201 response with the new task.
 	ctx.JSON(201, newTask)
 }
 
 func (tc *TaskController) UpdateTaskPut(ctx *gin.Context) {
 	newTask := models.Task{}
 
+	// Bind the request body to the newTask struct.
 	err := ctx.BindJSON(&newTask)
+
+	// If there is an error binding the request body, return a 400 response.
 	if err != nil {
 		ctx.JSON(400, gin.H{"error": "Invalid request"})
 		return
 	}
 
+	// If any of the required fields are missing, return a 400 response.
 	if newTask.Title == "" {
 		ctx.JSON(400, gin.H{"error": "title field is required"})
 		return
@@ -100,42 +120,58 @@ func (tc *TaskController) UpdateTaskPut(ctx *gin.Context) {
 		return
 	}
 
+	// Update the task using the TaskService.
 	taskID := ctx.Param("id")
 	task := tc.service.UpdateTask(taskID, &newTask)
+
+	// If the task is not found, return a 404 response.
 	if task == nil {
 		ctx.JSON(404, gin.H{"error": "Task not found"})
 		return
 	}
 
+	// Otherwise, return a 200 response with the updated task.
 	ctx.JSON(200, task)
 }
 
 func (tc *TaskController) UpdateTaskPatch(ctx *gin.Context) {
 	newTask := models.Task{}
 
+	// Bind the request body to the newTask struct.
 	err := ctx.BindJSON(&newTask)
+
+	// If there is an error binding the request body, return a 400 response.
 	if err != nil {
 		ctx.JSON(400, gin.H{"error": "Invalid request"})
 		return
 	}
 
+	// Update the task using the TaskService.
 	taskID := ctx.Param("id")
 	task := tc.service.UpdateTask(taskID, &newTask)
+
+	// If the task is not found, return a 404 response.
 	if task == nil {
 		ctx.JSON(404, gin.H{"error": "Task not found"})
 		return
 	}
 
+	// Otherwise, return a 200 response with the updated task.
 	ctx.JSON(200, task)
 }
 
 func (tc *TaskController) DeleteTask(ctx *gin.Context) {
 	taskID := ctx.Param("id")
+
+	// Delete the task using the TaskService.
 	err := tc.service.DeleteTask(taskID)
+
+	// If the task is not found, return a 404 response.
 	if err != nil {
 		ctx.JSON(404, gin.H{"error": "Task not found"})
 		return
 	}
 
+	// Otherwise, return a 204 response.
 	ctx.JSON(204, nil)
 }
--- task_manager/data/task_service.go
+++ task_manager/data/task_service.go
@@ -2,24 +2,26 @@ package data
 
 import (
 	"errors"
-	"fmt"
 	"math/rand"
 	"task_manager/models"
 )
 
+// TaskService is a struct that holds a map of tasks.
 type TaskService struct {
 	tasks map[string]*models.Task
 }
 
+// NewTaskService creates a new instance of TaskService.
+// It initializes the tasks map and returns a pointer to the TaskService.
 func NewTaskService() *TaskService {
 	return &TaskService{tasks: make(map[string]*models.Task)}
 }
 
+// GetTasks returns a slice of all tasks.
 func (ts *TaskService) GetTasks() []models.Task {
-	fmt.Println(ts.tasks)
-
 	tasks := []models.Task{}
 
+	// Loop through the tasks map and append each task to the tasks slice.
 	for _, task := range ts.tasks {
 		tasks = append(tasks, *task)
 	}
@@ -27,7 +29,10 @@ func (ts *TaskService) GetTasks() []models.Task {
 	return tasks
 }
 
+// GetTaskByID returns the task with the given ID.
 func (ts *TaskService) GetTaskByID(id string) *models.Task {
+	// Check if the task with the given ID exists in the tasks map.
+	// If it exists, return the task, otherwise return nil.
 	if task, ok := ts.tasks[id]; ok {
 		return task
 	}
@@ -35,20 +40,17 @@ func (ts *TaskService) GetTaskByID(id string) *models.Task {
 	return nil
 }
 
+// CreateTask creates a new task and adds it to the tasks map.
 func (ts *TaskService) CreateTask(task *models.Task) {
-	ts.tasks[task.ID] = &models.Task{
-		ID:          task.ID,
-		Title:       task.Title,
-		Description: task.Description,
-		DueDate:     task.DueDate,
-		Status:      task.Status,
-	}
+	ts.tasks[task.ID] = task
 }
 
+// randomString generates a random string of the given length.
 func (ts *TaskService) randomString(length int) string {
 	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 	b := make([]rune, length)
 
+	// Generate a random string of the given length using the letterRunes slice.
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
@@ -56,9 +58,12 @@ func (ts *TaskService) randomString(length int) string {
 	return string(b)
 }
 
+// GenerateID generates a random ID for a new task.
 func (ts *TaskService) GenerateID() string {
 	var id string
 
+	// Generate a random ID and check if it already exists in the tasks map.
+	// If it exists, generate a new ID until a unique one is found.
 	for {
 		id = ts.randomString(30)
 		if ts.GetTaskByID(id) == nil {
@@ -69,12 +74,15 @@ func (ts *TaskService) GenerateID() string {
 	return id
 }
 
+// UpdateTask updates the task with the given ID using the provided task data.
 func (ts *TaskService) UpdateTask(id string, taskData *models.Task) *models.Task {
 	_, ok := ts.tasks[id]
 	if !ok {
 		return nil
 	}
 
+	// Update the task fields with the provided task data.
+	// If a field is not provided, the existing value is retained.
 	if taskData.Title != "" {
 		ts.tasks[id].Title = taskData.Title
 	}
@@ -94,7 +102,11 @@ func (ts *TaskService) UpdateTask(id string, taskData *models.Task) *models.Task
 	return ts.tasks[id]
 }
 
+// DeleteTask deletes the task with the given ID.
 func (ts *TaskService) DeleteTask(id string) error {
+	// Check if the task with the given ID exists in the tasks map.
+	// If it exists, delete the task, otherwise return an error.
+
 	if _, ok := ts.tasks[id]; ok {
 		delete(ts.tasks, id)
 		return nil
